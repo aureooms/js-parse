ffefe432fd8989d921a824eb11f95b126f72379d
Aurélien Ooms <aurelien.ooms@gmail.com>
2016-07-28 09:14:56 +0200

    test skip

---
+++ test/src/skip.js
@@ -0,0 +1,31 @@
+import test from 'ava' ;
+
+require('babel-register')({ignore:['esprima','istanbul']});
+const { fromstring , skipblanks } = require('../../src/index.js') ;
+
+/**
+ * @test {parse#fromstring}
+ * @test {parse#skipblanks}
+ */
+test( 'can read from string' , t => {
+
+	const input = '\n\n\n    1 ,\t22\n, 333\t,\n-44 ,\t-5 \t\n  \t' ;
+	const stream = fromstring( input ) ;
+	t.deepEqual( skipblanks( stream ) , '1' ) ;
+	t.deepEqual( skipblanks( stream ) , ',' ) ;
+	t.deepEqual( skipblanks( stream ) , '2' ) ;
+	t.deepEqual( skipblanks( stream ) , '2' ) ;
+	t.deepEqual( skipblanks( stream ) , ',' ) ;
+	t.deepEqual( skipblanks( stream ) , '3' ) ;
+	t.deepEqual( skipblanks( stream ) , '3' ) ;
+	t.deepEqual( skipblanks( stream ) , '3' ) ;
+	t.deepEqual( skipblanks( stream ) , ',' ) ;
+	t.deepEqual( skipblanks( stream ) , '-' ) ;
+	t.deepEqual( skipblanks( stream ) , '4' ) ;
+	t.deepEqual( skipblanks( stream ) , '4' ) ;
+	t.deepEqual( skipblanks( stream ) , ',' ) ;
+	t.deepEqual( skipblanks( stream ) , '-' ) ;
+	t.deepEqual( skipblanks( stream ) , '5' ) ;
+	t.deepEqual( skipblanks( stream ) , null ) ;
+
+} ) ;
